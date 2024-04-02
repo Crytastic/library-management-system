1ee044402b8fd67d0da2fde69eae2198e3cda711
xsuchan6 <xsuchan6@fi.muni.cz>
2024-04-02 23:32:17 +0200

    Add executable spring boot application with test endpoint defined in openapi.yaml

--- book/src/main/java/cz/muni/fi/pa165/BookController.java
+++ book/src/main/java/cz/muni/fi/pa165/BookController.java
@@ -1,10 +1,16 @@
 package cz.muni.fi.pa165;
 
-/**
- * Hello world!
- */
-public class BookController {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import org.openapitools.api.BookApi;
+import org.openapitools.model.BookTestResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BookController implements BookApi {
+
+    @Override
+    public ResponseEntity<BookTestResponse> test() {
+        return new ResponseEntity<>(new BookTestResponse().message("Book microservice is ready"), HttpStatus.OK);
     }
 }
