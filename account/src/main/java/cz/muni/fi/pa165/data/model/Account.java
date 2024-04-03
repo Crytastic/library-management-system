33826c4f76baec6d2f7c44ce3334c6ef492d94de
xzapoto1 <514318@mail.muni.cz>
2024-04-03 18:51:33 +0200

    Add constructor, getters and setters to Account class

--- account/src/main/java/cz/muni/fi/pa165/data/model/Account.java
+++ account/src/main/java/cz/muni/fi/pa165/data/model/Account.java
@@ -10,11 +10,50 @@ public class Account {
 
     private String accountType;
 
-    private String name;
-
-    private String surname;
-
     private String address;
 
     private LocalDate birthDate;
+
+    public Account(String username, String accountType, String address, LocalDate birthDate) {
+        this.username = username;
+        this.accountType = accountType;
+        this.address = address;
+        this.birthDate = birthDate;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
 }
