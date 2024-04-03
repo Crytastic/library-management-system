19aaac3cd23022df656e5f42019d68dd3d6a4139
Maxmilián Šeffer <max.seffer@gmail.com>
2024-04-04 00:10:20 +0200

    Add GET /api/rentals endpoint to return all rentals

--- rental/src/main/java/cz/muni/fi/pa165/facade/RentalFacade.java
+++ rental/src/main/java/cz/muni/fi/pa165/facade/RentalFacade.java
@@ -1,8 +1,13 @@
 package cz.muni.fi.pa165.facade;
 
+import cz.muni.fi.pa165.dao.RentalDAO;
 import cz.muni.fi.pa165.service.RentalService;
+import org.openapitools.model.Rental;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class RentalFacade {
@@ -12,5 +17,20 @@ public class RentalFacade {
     @Autowired
     public RentalFacade(RentalService rentalService) {
         this.rentalService = rentalService;
+    }
+
+    public List<Rental> findAll() {
+        return rentalService
+                .findAll()
+                .stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+    }
+
+    private Rental convertToDTO(RentalDAO rentalDAO) {
+        return new Rental()
+                .book(rentalDAO.getBook())
+                .rentedBy(rentalDAO.getRentedBy())
+                .borrowDate(rentalDAO.getBorrowDate());
     }
 }
--- rental/src/main/java/cz/muni/fi/pa165/rest/RentalController.java
+++ rental/src/main/java/cz/muni/fi/pa165/rest/RentalController.java
@@ -1,16 +1,35 @@
 package cz.muni.fi.pa165.rest;
 
+import cz.muni.fi.pa165.facade.RentalFacade;
 import org.openapitools.api.RentalApi;
+import org.openapitools.model.Rental;
 import org.openapitools.model.RentalTestResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class RentalController implements RentalApi {
+
+    private final RentalFacade rentalFacade;
+
+    @Autowired
+    public RentalController(RentalFacade rentalFacade) {
+        this.rentalFacade = rentalFacade;
+    }
+
 
     @Override
     public ResponseEntity<RentalTestResponse> test() {
         return new ResponseEntity<>(new RentalTestResponse().message("Service running"), HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<List<Rental>> getRentals() {
+        return new ResponseEntity<>(rentalFacade.findAll(), HttpStatus.OK);
     }
 }
