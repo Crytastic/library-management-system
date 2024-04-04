05f9075722975dce0290490f2475ad035843d5eb
Maxmilián Šeffer <max.seffer@gmail.com>
2024-04-04 02:03:52 +0200

    Remove unnecessary imports, formatting

--- rental/src/main/java/cz/muni/fi/pa165/service/RentalService.java
+++ rental/src/main/java/cz/muni/fi/pa165/service/RentalService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.OffsetDateTime;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,7 +32,6 @@ public class RentalService {
         RentalDAO rentalDAO = new RentalDAO(book, rentedBy, borrowDate, expectedReturnDate, false, null);
         return rentalRepository.store(rentalDAO);
     }
-
 
     public Optional<RentalDAO> findById(Long id) {
         return rentalRepository.findById(id);
