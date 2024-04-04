d4d842a3654652d839c06df6df69b98bae04f9d0
Maxmilián Šeffer <max.seffer@gmail.com>
2024-04-04 22:55:31 +0200

    Cover BookController createBook valid request

---
+++ book/src/test/java/cz/muni/fi/pa165/rest/BookControllerTest.java
@@ -0,0 +1,45 @@
+package cz.muni.fi.pa165.rest;
+
+import cz.muni.fi.pa165.facade.BookFacade;
+import cz.muni.fi.pa165.util.BookDTOFactory;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.openapitools.model.BookDTO;
+import org.openapitools.model.BookStatus;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class BookControllerTest {
+
+    @Mock
+    private BookFacade bookFacade;
+
+    @InjectMocks
+    private BookController bookController;
+
+    @Test
+    void createBook_validRequestBody_createsBook() {
+        // Arrange
+        String title = "The Lord of the Rings";
+        String description = "Fantasy novel";
+        String author = "J.R.R. Tolkien";
+        BookStatus status = BookStatus.AVAILABLE;
+        BookDTO createdBook = BookDTOFactory.createBook(title, description, author, status);
+        when(bookFacade.createBook(title, description, author)).thenReturn(createdBook);
+
+        // Act
+        ResponseEntity<BookDTO> response = bookController.createBook(title, description, author);
+
+        // Assert
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody()).isEqualTo(createdBook);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+    }
+}
