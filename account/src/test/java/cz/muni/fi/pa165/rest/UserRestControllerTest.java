a1288a21ae7a4e3e4bfd0ba299c579431e8387ae
xsuchan6 <xsuchan6@fi.muni.cz>
2024-04-05 16:36:41 +0200

    Cover UserRestController updateUser with UTs

--- account/src/test/java/cz/muni/fi/pa165/rest/UserRestControllerTest.java
+++ account/src/test/java/cz/muni/fi/pa165/rest/UserRestControllerTest.java
@@ -1,5 +1,6 @@
 package cz.muni.fi.pa165.rest;
 
+import cz.muni.fi.pa165.exceptions.UnauthorisedException;
 import cz.muni.fi.pa165.exceptions.UsernameAlreadyExistsException;
 import cz.muni.fi.pa165.facade.UserFacade;
 import org.junit.jupiter.api.Test;
@@ -99,5 +100,55 @@ class UserRestControllerTest {
         verify(userFacade, times(1)).deleteById(idToDelete);
     }
 
+    @Test
+    void updateUser_incorrectCredentialsOrNoRights_returnsUnauthorizedStatus() {
+        String username = "programmer123";
+        String password = "password";
+        UserType userType = UserType.MEMBER;
+        String address = "Botanická 68a";
+        LocalDate birthDate = LocalDate.parse("2000-02-02");
+        Long id = 1L;
+
+        Mockito.when(userFacade.updateUser(id, username, password, address, birthDate, userType)).thenThrow(UnauthorisedException.class);
+
+        ResponseEntity<UserDTO> response = userRestController.updateUser(id, username, password, address, birthDate, userType);
+        assertThat(response.getBody()).isNull();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+    }
+
+    @Test
+    void updateUser_userUpdatesNotExistingUser_returnsNotFoundStatus() {
+        String username = "programmer123";
+        String password = "password";
+        UserType userType = UserType.MEMBER;
+        String address = "Botanická 68a";
+        LocalDate birthDate = LocalDate.parse("2000-02-02");
+        Long id = 1L;
+
+        Mockito.when(userFacade.updateUser(id, username, password, address, birthDate, userType)).thenReturn(Optional.empty());
+
+        ResponseEntity<UserDTO> response = userRestController.updateUser(id, username, password, address, birthDate, userType);
+        assertThat(response.getBody()).isNull();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    void updateUser_sufficientRightsAndUserExists_returnsUserAndOkStatus() {
+        String username = "programmer123";
+        String password = "password";
+        UserType userType = UserType.MEMBER;
+        String address = "Botanická 68a";
+        LocalDate birthDate = LocalDate.parse("2000-02-02");
+        Long id = 1L;
+        UserDTO user = new UserDTO().username(username).userType(userType).address(address).birthDate(birthDate);
+
+        Mockito.when(userFacade.updateUser(id, username, password, address, birthDate, userType)).thenReturn(Optional.of(user));
+
+        ResponseEntity<UserDTO> response = userRestController.updateUser(id, username, password, address, birthDate, userType);
+
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody()).isEqualTo(user);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
 
 }
