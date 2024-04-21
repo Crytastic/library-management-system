4a1887914d24698cdee085d5cdadd5c69592a15c
Maxmilián Šeffer <max.seffer@gmail.com>
2024-04-21 17:49:16 +0200

    Add WebMvcTest for negative case of GET /api/rentals/{id}

--- rental/src/test/java/cz/muni/fi/pa165/rest/RentalControllerWebMvcTest.java
+++ rental/src/test/java/cz/muni/fi/pa165/rest/RentalControllerWebMvcTest.java
@@ -3,9 +3,6 @@ package cz.muni.fi.pa165.rest;
 import cz.muni.fi.pa165.facade.RentalFacade;
 import cz.muni.fi.pa165.util.ObjectConverter;
 import cz.muni.fi.pa165.util.RentalDTOFactory;
-
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalUnit;
 import org.junit.jupiter.api.Test;
 import org.openapitools.model.RentalDTO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,10 +14,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
 import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
 import java.util.Optional;
 
-import static java.util.Calendar.SECOND;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -71,4 +66,20 @@ public class RentalControllerWebMvcTest {
         assertThat(response).isEqualTo(rentalDTO);
     }
 
+    @Test
+    void updateRental_invalidId_returnsNotFound() throws Exception {
+        // Arrange
+        Long id = 10L;
+        when(rentalFacade.findById(id)).thenReturn(Optional.empty());
+
+        // Act and Assert
+        String responseJson = mockMvc.perform(get("/api/rentals/{id}", id)
+                        .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isNotFound())
+                .andReturn()
+                .getResponse()
+                .getContentAsString(StandardCharsets.UTF_8);
+
+        assertThat(responseJson).isEqualTo("");
+    }
 }
