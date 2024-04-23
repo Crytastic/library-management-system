869f535c118cdbf7f20e9d88d868475eb1093316
xsuchan6 <xsuchan6@fi.muni.cz>
2024-04-23 13:05:55 +0200

    Add different types of custom exceptions

---
+++ infrastructure/src/main/java/cz/muni/fi/pa165/exceptionhandling/exceptions/ConstraintViolationException.java
@@ -0,0 +1,17 @@
+package cz.muni.fi.pa165.exceptionhandling.exceptions;
+
+public class ConstraintViolationException extends RuntimeException {
+    public ConstraintViolationException() {}
+
+    public ConstraintViolationException(String message) {
+        super(message);
+    }
+
+    public ConstraintViolationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ConstraintViolationException(Throwable cause) {
+        super(cause);
+    }
+}
