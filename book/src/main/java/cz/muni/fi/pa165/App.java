af61a58a1f38777a3fb95402aee39f0b00117964
xsuchan6 <xsuchan6@fi.muni.cz>
2024-05-07 23:17:15 +0200

    WIP: Add Oauth2 security into book service (not working yet)

--- book/src/main/java/cz/muni/fi/pa165/App.java
+++ book/src/main/java/cz/muni/fi/pa165/App.java
@@ -1,11 +1,60 @@
 package cz.muni.fi.pa165;
 
+import io.swagger.v3.oas.models.security.OAuthFlow;
+import io.swagger.v3.oas.models.security.OAuthFlows;
+import io.swagger.v3.oas.models.security.Scopes;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
+@EnableWebSecurity
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(x -> x
+                        .requestMatchers(HttpMethod.GET, "/api/books").hasAuthority("SCOPE_book_read")
+                        .requestMatchers(HttpMethod.POST, "/api/books").hasAuthority("SCOPE_book_write")
+                        .anyRequest().permitAll()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.opaqueToken(Customizer.withDefaults()))
+        ;
+        return http.build();
+    }
+
+    @Bean
+    public OpenApiCustomizer openAPICustomizer() {
+        return openApi -> openApi.getComponents()
+                .addSecuritySchemes("MUNI",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.OAUTH2)
+                                .description("get access token with OAuth 2 Authorization Code Grant")
+                                .flows(new OAuthFlows()
+                                        .authorizationCode(new OAuthFlow()
+                                                .authorizationUrl("https://oidc.muni.cz/oidc/authorize")
+                                                .tokenUrl("https://oidc.muni.cz/oidc/token")
+                                                .scopes(new Scopes()
+                                                        .addString("book_read", "reading events")
+                                                        .addString("book_write", "creating events")
+                                                        .addString("test_1", "Nothing")
+                                                        .addString("test_2", "Nothing")
+                                                        .addString("test_read", "Nothing")
+                                                        .addString("test_write", "Nothing")
+                                                )
+                                        )
+                                )
+                );
     }
 }
