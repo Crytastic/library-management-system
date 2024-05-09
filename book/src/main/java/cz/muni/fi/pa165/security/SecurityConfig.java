b3a3b1312a2ba6c46e2f4b51fac56e88e86b7d49
xsuchan6 <xsuchan6@fi.muni.cz>
2024-05-09 21:27:15 +0200

    Change swagger UI Authorization form to enter already obtained access token instead of client_id and client_secret

--- book/src/main/java/cz/muni/fi/pa165/security/SecurityConfig.java
+++ book/src/main/java/cz/muni/fi/pa165/security/SecurityConfig.java
@@ -1,12 +1,11 @@
 package cz.muni.fi.pa165.security;
 
-import io.swagger.v3.oas.models.security.OAuthFlow;
-import io.swagger.v3.oas.models.security.OAuthFlows;
-import io.swagger.v3.oas.models.security.Scopes;
-import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -17,12 +16,14 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@OpenAPIDefinition
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 public class SecurityConfig {
-
-    private static final Logger log = LoggerFactory.getLogger(SecurityConfig.class);
-    private static final String SECURITY_SCHEME_OAUTH2 = "MUNI";
-    private static final String SECURITY_SCHEME_BEARER = "Bearer";
-    public static final String SECURITY_SCHEME_NAME = SECURITY_SCHEME_OAUTH2;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -40,31 +41,5 @@ public class SecurityConfig {
                 .oauth2ResourceServer(oauth2 -> oauth2.opaqueToken(Customizer.withDefaults()))
         ;
         return http.build();
-    }
-
-    @Bean
-    public OpenApiCustomizer openAPICustomizer() {
-        return openApi -> {
-            log.info("adding security to OpenAPI description");
-            openApi.getComponents()
-
-                    .addSecuritySchemes(SECURITY_SCHEME_OAUTH2,
-                            new SecurityScheme()
-                                    .type(SecurityScheme.Type.OAUTH2)
-                                    .description("get access token with OAuth 2 Authorization Code Grant")
-                                    .flows(new OAuthFlows()
-                                            .authorizationCode(new OAuthFlow()
-                                                    .authorizationUrl("https://oidc.muni.cz/oidc/authorize")
-                                                    .tokenUrl("https://oidc.muni.cz/oidc/token")
-                                                    .scopes(new Scopes()
-                                                            .addString("test_read", "reading events")
-                                                            .addString("test_write", "creating events")
-                                                            .addString("test_1", "deleting events")
-                                                    )
-                                            )
-                                    )
-                    )
-            ;
-        };
     }
 }
