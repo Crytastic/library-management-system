83db46f7f90c396362987aebdcf312a42a55c2bf
xsuchan6 <xsuchan6@fi.muni.cz>
2024-05-10 00:35:51 +0200

    Add OAuth2 BorrowingSecurityConfig, fix UTs so they dont require authentication

---
+++ borrowing/src/main/java/cz/muni/fi/pa165/security/BorrowingSecurityConfig.java
@@ -0,0 +1,36 @@
+package cz.muni.fi.pa165.security;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@OpenAPIDefinition
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class BorrowingSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.GET, "/api/borrowings/**").hasAuthority("SCOPE_read")
+                        .requestMatchers(HttpMethod.DELETE, "/api/borrowings/**").hasAuthority("SCOPE_test1")
+                        .requestMatchers(HttpMethod.PATCH, "/api/borrowings/**").hasAuthority("SCOPE_write")
+                        .requestMatchers(HttpMethod.POST, "/api/borrowings/**").hasAuthority("SCOPE_write")
+                        .anyRequest().permitAll())
+                .oauth2ResourceServer(oauth2 -> oauth2.opaqueToken(Customizer.withDefaults()));
+        return http.build();
+    }
+}
