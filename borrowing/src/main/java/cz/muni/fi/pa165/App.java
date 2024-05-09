60da09c8cc2d70ab423ea5d6bc4df126e3de15eb
xsuchan6 <xsuchan6@fi.muni.cz>
2024-05-10 00:44:50 +0200

    Add info logs to Borrowing service

--- borrowing/src/main/java/cz/muni/fi/pa165/App.java
+++ borrowing/src/main/java/cz/muni/fi/pa165/App.java
@@ -1,10 +1,25 @@
 package cz.muni.fi.pa165;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.context.ServletWebServerInitializedEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class App {
+
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+
+    @EventListener
+    public void onApplicationEvent(ServletWebServerInitializedEvent event) {
+        log.info("**************************");
+        int port = event.getWebServer().getPort();
+        log.info("visit http://localhost:{}/swagger-ui.html for UI", port);
+        log.info("visit http://localhost:{}/openapi.yaml for OpenAPI document", port);
+        log.info("**************************");
+    }
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
